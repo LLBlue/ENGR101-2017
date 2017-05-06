a2ea00d715e313f30687163f09d58320297d397a
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-06 13:45:59 +1200

    Robot Functions Header

---
+++ RobotFunction.h
@@ -0,0 +1,9 @@
+#ifndef ROBOTFUNCTION_H_INCLUDED
+#define ROBOTFUNCTION_H_INCLUDED
+int see();
+int driveFoward(int time_microseconds);
+int turn_left (int time_microseconds);
+int turn_right (int time_microseconds);
+int slowBackward(int time_microseconds);
+int see_and_reverse();
+#endif
