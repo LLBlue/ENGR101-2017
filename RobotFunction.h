e2bd27fe2b870ef6aedad215dabd3749e1a1a101
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-08 13:30:32 +1200

    added gate sequence

--- RobotFunction.h
+++ RobotFunction.h
@@ -7,6 +7,7 @@ int turn_right (int time_microseconds);
 int slowBackward(int time_microseconds);
 int see_and_reverse();
 int seeLineX(int Y);
+int gateSequence ();
 int testSee();
 int testPrint();
 #endif
