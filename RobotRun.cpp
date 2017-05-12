2c2bda9ba7439150a2b53d363a10d2e3b5c64a88
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-12 14:03:54 +1200

    Added Q3 logic

--- RobotRun.cpp
+++ RobotRun.cpp
@@ -21,20 +21,54 @@ int main() {
   //Q1 open gate logic
   gateSequence();
   //Q2 follow line logic
+  char Q2 = 0;
+  while (Q2 == 0) {
+    double speedMultiplier = 0.5; //change this during testing
+    int linePosition = 120; //change line position
+    lineError = seeLineX(linePosition); 
+    if (lineError == 100000) {
+      //go backwards
+      slowBackward(100000);
+      sleep1(0,10000);
+    } else if (lineError == 100001) {
+      Q2 = 1;
+    } else {
+      setSpeed(lineError*speedMultiplier) 
+      sleep1(0,10000);
+    }
+  }
+  //Q3 solve line maze
+  //need to make it check for a red square at some point for Q4
   double speedMultiplier = 0.5; //change this during testing
-  int linePosition;
-  lineError = seeLineX(120); //change line position 
+  int linePositionX=120; //change line position
+  int linePositionLeft = 80; //change where it looks for the left and right lines
+  int linePositionRight 240;
+  int lineError = seeLineX(linePositionX);
+  int lineOutputLeft;
+  int lineOutputRight;
   if (lineError == 100000) {
-    //go backwards
+    //if no more line ahead then try turning left or right
+    lineOutputLeft = seeLineY(linePositionLeft);
+    lineOutputRight = seeLineY(linePositionRight);
+    if(lineOutputLeft != 100000) {
+      //turn left
+      turn_left(100000);
+    } else if (lineOutputRight !=100000) {
+      //turn right
+      turn_right(100000);
+    } else {
+      //go backwards
+      slowBackward(100000);
+    }
     sleep1(0,10000);
-  } else if (lineError == 100001) {
-    //goto Q3
   } else {
     setSpeed(lineError*speedMultiplier) 
     sleep1(0,10000);
   }
-  //Q3 solve line maze
   //Q4 solve wall maze
+  
+  
   return 0;
 }
 */
+
