3704958c3907dbc7be23991ce492b1027e737e48
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-17 17:52:57 +1200

    Update RobotFunction.h

--- RobotFunction.h
+++ RobotFunction.h
@@ -1,16 +1,11 @@
 #ifndef ROBOTFUNCTION_H_INCLUDED
 #define ROBOTFUNCTION_H_INCLUDED
-int see();
-int driveFoward(int time_microseconds);
-int turn_left (int time_microseconds);
-int turn_right (int time_microseconds);
-int slowBackward(int time_microseconds);
-int see_and_reverse();
+int seeIR();
+int slowBackward(int timeSeconds, int timeMicroseconds);
 int seeLineX(int Y);
 int seeLineY(int X);
 int setSpeed(int speedFactor);
 int gateSequence ();
-int followWall();
 int detectLeftWall();
 int detectRightWall();
 int seeWallFront();
