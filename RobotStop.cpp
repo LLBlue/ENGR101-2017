144e569aa6930be4c741791608cca1a965135332
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-17 17:23:45 +1200

    Use to stop the bot

---
+++ RobotStop.cpp
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include  <time.h>
+#include "E101.h"
+
+/* Stops robot
+ * doesn't call init() as that was already called and is why we're still running
+ */
+int  main(){
+  stop(1);
+  sleep1(0, 100);
+  stop(2);   
+  sleep1(1, 100);
+  return  0;
+}
+
+/* Put this in the home directory and compile it with
+ *g++ RobotStop.cpp -le101 -o stop
+ *chmod +x stop
+ *./stop
+ */
