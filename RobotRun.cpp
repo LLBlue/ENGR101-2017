953d1e435958119bf34e219f75d9eb57768ee4d5
ryan-turnerNZ <ryanturnernz@gmail.com>
2017-05-17 13:08:41 +1200

    Update RobotRun.cpp

--- RobotRun.cpp
+++ RobotRun.cpp
@@ -9,7 +9,33 @@
 */
 int  main(){
   init();
-  gateSequence ();
+  
+  
+  gateSequence();
+  sleep1(1,0);
+  
+  
+  char Q2 = 0;
+  while (Q2 == 0) {
+    double speedMultiplier = 0.5; //change this during testing
+    int linePosition = 120; //change line position
+    int lineError = seeLineX(linePosition); 
+    if (lineError == 100000) {
+      //go backwards
+      slowBackward(100000);
+      sleep1(0,10000);
+    } else if (lineError == 100001) {
+      Q2 = 1;
+    } else {
+      int speedGo = lineError*speedMultiplier;
+      setSpeed(speedGo);
+      sleep1(0,10000);
+    }
+  }
+  
+  
+  
+   
   return  0;
 }
 /* Real Code
