264887320770f85ac39e4eabf227c7ca9bbea567
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-18 13:54:30 +1200

    Update RobotFunction.h

--- RobotFunction.h
+++ RobotFunction.h
@@ -1,6 +1,8 @@
 #ifndef ROBOTFUNCTION_H_INCLUDED
 #define ROBOTFUNCTION_H_INCLUDED
 int seeIR();
+int turnLeft (int seconds, int microseconds);
+int turnRight (int seconds, int microseconds);
 int slowBackward(int timeSeconds, int timeMicroseconds);
 int seeLineX(int Y);
 int seeLineY(int X);
