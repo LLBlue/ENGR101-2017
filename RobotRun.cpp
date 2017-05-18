f5c23b3760bc4fdd7ed57140279c4e0c9a07f934
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-18 15:32:24 +1200

    Update RobotRun.cpp

--- RobotRun.cpp
+++ RobotRun.cpp
@@ -106,55 +106,93 @@ int  main(){
 */
 /*
 int main() {
+  bool debug = true;
   init();
+  
+  
   //Q1 open gate logic
   gateSequence();
   sleep1(1,0);
+  
+  
   //Q2 follow line logic
   char Q2 = 0;
   while (Q2 == 0) {
-    double speedMultiplier = 0.5; //change this during testing
+    double speedMultiplier = .95; //change this during testing
     int linePosition = 120; //change line position
     int lineError = seeLineX(linePosition); 
     if (lineError == 100000) {
+      //go backwards
       slowBackward(0, 100000);
       sleep1(0,10000);
     } else if (lineError == 100001) {
       Q2 = 1;
     } else {
-      int speedGo = lineError*speedMultiplier;
+      int speedGo = int ((double)lineError*speedMultiplier);
       setSpeed(speedGo);
       sleep1(0,10000);
     }
   }
+  
+  
   //Q3 solve line maze
   //need to make it check for a red square at some point for Q4
-  double speedMultiplier = 0.5; //change this during testing
-  int linePositionX=120; //change line position
+  double speedMultiplier = 0.95; //change this during testing
+  int linePositionX=230; //change line position
   int linePositionLeft = 80; //change where it looks for the left and right lines
-  int linePositionRight 240;
-  int lineError = seeLineX(linePositionX);
+  int linePositionRight = 120;
+  int lineError;
   int lineOutputLeft;
   int lineOutputRight;
-  if (lineError == 100000) {
-    //if no more line ahead then try turning left or right
-    lineOutputLeft = seeLineY(linePositionLeft);
-    lineOutputRight = seeLineY(linePositionRight);
-    if(lineOutputLeft != 100000) {
-      //turn left
-      turn_left(100000);
-    } else if (lineOutputRight !=100000) {
-      //turn right
-      turn_right(100000);
-    } else {
-      //go backwards
-      slowBackward(100000);
+  char Q3 = 0;
+  while(Q3==0) {
+    lineError = seeLineX(linePositionX);
+	  if(debug) {
+      printf("Line Error: %d", lineError);
     }
+    if (lineError == 100000) {
+      //if no more line ahead then try turning left or right
+      lineOutputLeft = seeLineY(linePositionLeft);
+      if(debug) {
+        printf("output left: %d\n", lineOutputLeft);
+      }
+      lineOutputRight = seeLineY(linePositionRight);
+      if(debug){
+        printf("output right: %d\n", lineOutputRight);
+      }
+      if(lineOutputLeft != 100000) {
+        //turn left
+        if(debug){
+	  printf("turning left\n");
+        }
+        turnLeft(0, 625000);
+      } else if (lineOutputRight !=100000) {
+        //turn right
+        if(debug){
+  	  printf("turning right\n");
+        }
+        turnRight(1, 250000);
+      } else {
+        //turn around
+        turnLeft(1, 250000);
+        if(debug){
+          printf("turning around\n");
+        }
+      }
     sleep1(0,10000);
-  } else {
-    setSpeed(lineError*speedMultiplier) 
-    sleep1(0,10000);
+    } else {
+      if(lineError == 100001) {
+        lineError = 0;
+      }
+      if(debug){
+       printf("going straigt\n");
+      }
+      setSpeed(lineError*speedMultiplier) ;
+      sleep1(0,10000);
+    }
   }
+  
+  
   //Q4 solve wall maze
   
 	int wallFront;
