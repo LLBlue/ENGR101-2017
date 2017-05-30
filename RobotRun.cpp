7ca54c9b92258ea6bc6f5332ab05b2629d54a29c
LLBlue <chanaudr1@myvuw.ac.nz>
2017-05-30 17:51:10 +1200

    Update RobotRun.cpp

--- RobotRun.cpp
+++ RobotRun.cpp
@@ -5,9 +5,19 @@
 
 int main() {
 	init();
+	bool debug = false;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
 	int wallLeft;
 	int wallRight;
-	bool debug = false;
 	int wallFront;
 	int redLineValue;
 	int linePositionX = 120;
@@ -86,7 +96,7 @@ int main() {
 	}	
 	return 0;
 }
-
+*/
 			
 			
 			
@@ -110,142 +120,182 @@ int main() {
 			
 			
 			
-  /*
+  
   //Q1 open gate logic
-  gateSequence();
-  sleep1(1,0);
+	gateSequence();
+	sleep1(1,0);
   
   
   //Q2 follow line logic
-  char Q2 = 0;
-  while (Q2 == 0) {
-    double speedMultiplier = .95; //change this during testing
-    int linePosition = 120; //change line position
-    int lineError = seeLineX(linePosition); 
-    if (lineError == 100000) {
-      //go backwards
-      slowBackward(0, 100000);
-      sleep1(0,10000);
-    } else if (lineError == 100001) {
-      Q2 = 1;
-    } else {
-      int speedGo = int ((double)lineError*speedMultiplier);
-      setSpeed(speedGo);
-      sleep1(0,10000);
-    }
-  }
+	char Q2 = 0;
+	while (Q2 == 0) {
+		double speedMultiplier = .95; //change this during testing
+		int linePosition = 120; //change line position
+		int lineError = seeLineX(linePosition); 
+		if (lineError == 100000) {
+			//go backwards
+			slowBackward(0, 100000);
+			sleep1(0,10000);
+		} else if (lineError == 100001) {
+			Q2 = 1;
+		} else {
+			int speedGo = int ((double)lineError*speedMultiplier);
+			setSpeed(speedGo);
+			sleep1(0,10000);
+		}
+	}
   
   
   //Q3 solve line maze
   //need to make it check for a red square at some point for Q4
-  double speedMultiplier = 0.95; //change this during testing
-  int linePositionX=120; //change line position
-  int linePositionLeft = 80; //change where it looks for the left and right lines
-  int linePositionRight = 120;
-  int lineError;
-  int lineOutputLeft;
-  int lineOutputRight;
-  int redLineValue;
-  char Q3 = 0;
-  while(Q3==0) {
-    lineError = seeLineX(linePositionX);
-	  if(debug) {
-      printf("Line Error: %d", lineError);
-    }
-    if (lineError == 100000) {
-      //if no more line ahead then try turning left or right
-      lineOutputLeft = seeLineY(linePositionLeft);
-      if(debug) {
-        printf("output left: %d\n", lineOutputLeft);
-      }
-      lineOutputRight = seeLineY(linePositionRight);
-      if(debug){
-        printf("output right: %d\n", lineOutputRight);
-      }
-      if(lineOutputLeft != 100000) {
-        //turn left
-        if(debug){
-	        printf("turning left\n");
-        }
-        turnLeft(0, 100000);
-        while(seeLineX(linePositionX)== 100000 || seeLineX(linePositionX) <= -10) {
-          turnLeft(0, 30000);
-        }
-      } else if (lineOutputRight !=100000) {
-        //turn right
-        if(debug){
-  	      printf("turning right\n");
-        }
-        turnRight(0, 100000);
-        while(seeLineX(linePositionX)== 100000 || seeLineX(linePositionX) >= 10) {
-          turnRight(0, 30000);
-        }
-      } else {
-        //turn around
-        turnRight(0, 100000);
-        while(seeLineX(linePositionX)== 100000 || seeLineX(linePositionX) >= 10) {
-          turnRight(0, 30000);
-        }
-        if(debug){
-          printf("turning around\n");
-        }
-      }
-    sleep1(0,10000);
-    } else {
-      if(lineError == 100001) {
-        lineError = 0;
-      }
-      if(debug){
-       printf("going straigt\n");
-      }
-      setSpeedSlow(lineError*speedMultiplier) ;
-      sleep1(0,10000);
-    }
-	  
-	  redLineValue = seeRedLine(linePositionX);
-	  if (redLineValue == 100002){
-		Q3 = 1;
-	  }
-  }
-
-
-
-
-	int wallFront;
-	int wallLeft;
-	int wallRight;
-	//int redLineValue;
-	linePositionX = 120;
-	char Q4 = 0;
-	while(Q4 == 0){
-		wallFront = seeIR(2); //Checks distance of wall in front
-		//printf("Wall Front: %d\n", wallFront);
-		if(wallFront >= 200){ //520 is distance we want to turn at
-			//printf("Turning\n");
-			wallLeft = seeIR(3); //Checks distance of wall at left
-			wallRight = seeIR(1); //Checks distance of wall at right
-			//if (wallLeft >= 400 && wallRight >= 400){ //more than 400 means wall there
-			//	turnLeftIR();
-			//}
-			if (wallLeft < wallRight){ //Turn left
-				turnLeftIR();
+	double speedMultiplier = 0.95; //change this during testing
+	int linePositionX=120; //change line position
+	int linePositionLeft = 80; //change where it looks for the left and right lines
+	int linePositionRight = 120;
+	int lineError;
+	int lineOutputLeft;
+	int lineOutputRight;
+	int redLineValue;
+	char Q3 = 0;
+	while(Q3==0) {
+		lineError = seeLineX(linePositionX);
+		if(debug) {
+			printf("Line Error: %d", lineError);
+		}
+		if (lineError == 100000) {//if no more line ahead then try turning left or right
+			lineOutputLeft = seeLineY(linePositionLeft);
+			if(debug) {
+				printf("output left: %d\n", lineOutputLeft);
 			}
-			else if (wallRight < wallLeft){ //Code for if wall on left but not right
-				turnRightIR();
+			lineOutputRight = seeLineY(linePositionRight);
+			if(debug){
+				printf("output right: %d\n", lineOutputRight);
 			}
+			if(lineOutputLeft != 100000) {//turn left
+				if(debug){
+					printf("turning left\n");
+				}
+				turnLeft(0, 100000);
+				while(seeLineX(linePositionX)== 100000 || seeLineX(linePositionX) <= -10) {
+					turnLeft(0, 30000);
+				}
+			} else if (lineOutputRight !=100000) {//turn right
+				if(debug){
+					printf("turning right\n");
+				}
+				turnRight(0, 100000);
+				while(seeLineX(linePositionX)== 100000 || seeLineX(linePositionX) >= 10) {
+					turnRight(0, 30000);
+				}
+			} else {//turn around
+				turnRight(0, 100000);
+				while(seeLineX(linePositionX)== 100000 || seeLineX(linePositionX) >= 10) {
+					turnRight(0, 30000);
+				}
+				if(debug){
+					printf("turning around\n");
+				}
+			}
+			sleep1(0,10000);
 		} else {
-			
-			
-			set_motor(1, 80);
-  			sleep1(0, 100);
-  			set_motor(2, 80);
-  			sleep1(0, 100000);
-			
-			
-			
+			if(lineError == 100001) {
+				lineError = 0;
+			}
+			if(debug){
+				printf("going straigt\n");
+			}
+			setSpeedSlow(lineError*speedMultiplier) ;
+			sleep1(0,10000);
+		}
+	  
+		redLineValue = seeRedLine(linePositionX);
+		if (redLineValue == 100002){
+			Q3 = 1;
 		}
 	}
+
+
+
+
+	int wallLeft;
+	int wallRight;
+	int wallFront;
+	//int redLineValue;
+	linePositionX = 120;
+	bool redLine = false;
+	bool waiting = true;
+	while(true){
+		while(!redLine){
+			wallFront = seeIR(2); //Checks distance of wall in front
+			if(wallFront >= 220){ //220 is distance we want to turn at
+				wallLeft = seeIR(3); //Checks distance of wall at left
+				wallRight = seeIR(1); //Checks distance of wall at right
+			
+				if (wallLeft > 200){ //Turn Right (Possibly need smaller value)
+					turnRightIR();
+				}
+				else if (wallRight > 200){ //Turn Left (Possibly need smaller value)
+					turnLeftIR();
+				}
+			} else {
+				wallLeft = seeIR(3); //Checks distance of wall at left
+				wallRight = seeIR(1); //Checks distance of wall at right
+				if(wallLeft > 600){
+					while(wallLeft > 550){
+						set_motor(2, 140);
+						sleep1(0, 100);
+						set_motor(1, -140);
+						sleep1(0,10000);
+						wallLeft = seeIR(3);
+					}
+				
+				}
+				if(wallRight > 600){
+					while(wallRight > 550){
+						set_motor(1, 140);
+						sleep1(0, 100);
+						set_motor(2, -140);
+						sleep1(0,10000);
+						wallRight = seeIR(1);
+					}
+				}else{
+				
+					set_motor(1, 80);
+  					sleep1(0, 100);
+  					set_motor(2, 80);
+  					sleep1(0, 100000);
+					redLineValue = seeRedLine(linePositionX);
+	 				if (redLineValue == 100002){
+						redLine = true;
+					}
+				}
+			}
+		}
+		printf("Red shit\n");
+		set_motor(1, 0);
+		sleep1(0,100);
+		set_motor(2, 0);
+		wallFront = seeIR(2);
+		while(waiting){
+			if(wallFront > 200){ //Need different value
+				while(wallFront > 150){
+					sleep1(0, 20000);
+					wallFront= seeIR(2);
+					printf("Seen Gate Waiting\n");
+				}
+				waiting = false;	
+			} else if(wallFront < 151){
+				sleep1(0,20000);
+				printf("Waiting for gate\n");
+			}
+			wallFront= seeIR(2);
+		}
+		redLine = false;
+		setSpeed(0);
+		sleep1(0, 200000);
+		printf("Moving on\n");
+	}	
 	
 	return 0;
 }
-*/
+
